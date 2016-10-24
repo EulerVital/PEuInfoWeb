394fbb60013942344b192fb1e320d3e2f8a0a126
User <User@DESKTOP-URPHR1B>
2016-10-23 23:14:00 -0200

    Add project files.

---
+++ PEuInfoWeb/Camadas/DAO/DBUtil/dConexao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PEuInfoWeb.Camadas.DAO.DBUtil
+{
+    public class dConexao
+    {
+        private SqlConnection conexao;
+
+        public SqlConnection Conectar()
+        {
+            conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["strConnection"].ConnectionString);
+            return conexao;
+        }
+    }
+}
---
+++ PEuInfoWeb/Camadas/DAO/dAdm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PEuInfoWeb.Camadas.DAO
+{
+    public class dAdm
+    {
+
+    }
+}
